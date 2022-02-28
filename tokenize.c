3e847c86ae20c71c463169be30240adca181c982
mebukibu <masa1280g@gmail.com>
2022-02-28 21:25:13 +0900

    1文字のローカル変数の実装

--- tokenize.c
+++ tokenize.c
@@ -26,13 +26,22 @@ void error_at(char *loc, char *fmt, ...) {
   exit(1);
 }
 
-// Consume the current token if it matches 'op'.
+// Consumes the current token if it matches 'op'.
 bool consume(char *op) {
   if (token->kind != TK_RESERVED || strlen(op) != token->len ||
       memcmp(token->str, op, token->len))
     return false;
   token = token->next;
   return true;
+}
+
+// Consumes the current token if it is an identifier.
+Token *consume_ident() {
+  if (token->kind != TK_IDENT)
+    return NULL;
+  Token *t = token;
+  token = token->next;
+  return t;
 }
 
 // Ensure that current token is 'op'.
@@ -108,8 +117,14 @@ Token *tokenize() {
         }
 
     // Single-letter punctuator
-    if (strchr("+-*/()<>;", *p)) {
+    if (strchr("+-*/()<>;=", *p)) {
       cur = new_token(TK_RESERVED, cur, p++, 1);
+      continue;
+    }
+
+    // Identifier
+    if ('a' <= *p && *p <= 'z') {
+      cur = new_token(TK_IDENT, cur, p++, 1);
       continue;
     }
 
