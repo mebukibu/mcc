39a7cec72efd8a2010f4966d1397cdadf801b282
mebukibu <masa1280g@gmail.com>
2022-03-07 13:40:50 +0900

    Statement expression の追加
    Statement expressionは，GNUの拡張の1つ
    ex.) return ({ int x=3; x}); => 3

--- parse.c
+++ parse.c
@@ -305,7 +305,6 @@ Node *stmt() {
     return node;
   }
 
-
   if (is_typename())
     return declaration();
 
@@ -421,6 +420,25 @@ Node* postfix() {
   return node;
 }
 
+// stmt-expr = "(" "{" stmt stmt* "}" ")"
+//
+// Statement expression is a GNU extention.
+Node *stmt_expr(Token *tok) {
+  Node *node = new_node(ND_STMT_EXPR, tok);
+  node->body = stmt();
+  Node *cur = node->body;
+
+  while (!consume("}")) {
+    cur->next = stmt();
+    cur = cur->next;
+  }
+  expect(")");
+
+  if (cur->kind != ND_EXPR_STMT)
+    error_tok(cur->tok, "stmt expr returning void is not supported");
+  *cur = *cur->lhs;
+  return node;
+}
 
 // func-args = "(" (assign ("," assign)*)? ")"
 Node *func_args() {
@@ -437,12 +455,19 @@ Node *func_args() {
   return head;
 }
 
-// primary = "(" expr ")" | "sizeof" unary | ident func-args? | str | num
-// args = "(" ident ("," ident)* ")"
+// primary = "(" "{" stmt-expr-tail
+//         | "(" expr ")"
+//         | "sizeof" unary
+//         | ident func-args?
+//         | str
+//         | num
 Node *primary() {
   Token *tok;
 
-  if (consume("(")) {
+  if (tok = consume("(")) {
+    if (consume("{"))
+      return stmt_expr(tok);
+    
     Node *node = expr();
     expect(")");
     return node;
